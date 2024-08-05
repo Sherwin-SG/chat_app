b812220431a9f2c49893d5a1ec50d3a61b8b261d
Sherwin Gonsalves <sherwingonsalves.sg@gmail.com>
2024-08-06 04:49:26 +0530

    l

--- app/components/GroupChatWindow.tsx
+++ app/components/GroupChatWindow.tsx
@@ -110,10 +110,10 @@ const GroupChatWindow: React.FC<GroupChatWindowProps> = ({ group, userEmail }) =
               className={`flex ${message.senderEmail === userEmail ? 'justify-end' : 'justify-start'}`}
             >
               <div
-                className={`p-2 rounded-lg  ${
+                className={`p-3 rounded-lg max-w-xs  ${
                   message.senderEmail === userEmail
                     ? 'bg-blue-500 text-white'
-                    : 'bg-gray-200 text-black dark:bg-gray-700 dark:text-white border-gray-300 dark:border-gray-600'
+                    : 'bg-gray-200 text-gray-800'
                 }`}
               >
                 <p className="whitespace-pre-wrap">
